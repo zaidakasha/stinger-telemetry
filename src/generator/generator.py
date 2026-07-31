b38ac87a275f6920f0123e24247aa94cf0bf2c8c
zaidakasha <zaidakasha2@gmail.com>
2026-07-31 20:30:03 +0100

    v0: channel-based reading generation for coolant and rpm

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -1,11 +1,21 @@
 import time
 import random
-def make_reading():
+def make_reading(sensor):
     dict_readings = {}
-    dict_readings['channel'] = 'coolant temp'
+    dict_readings['channel'] = str(sensor)
     dict_readings['timestamp'] = time.time()
-    dict_readings['value'] = random.gauss(85, 1 )
+    if sensor == 'coolant_temp':
+        dict_readings['value'] = random.gauss(85, 1 )
+    elif sensor == 'rpm':
+        rpm = 2000
+        dict_readings['value'] = rpm + random.uniform(-200,200)
     dict_readings['seq'] = 0
     return dict_readings
 
-print(make_reading())
+try:
+    while True:
+        print(make_reading('rpm'))
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print('Stopped')
